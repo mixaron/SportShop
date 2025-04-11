ea98ff850b5499a65156dd07b3f476f2adaf7ab5
Anton Ryabchuk <tosha.ryabchuk@gmail.com>
2025-04-11 18:27:20 +0400

    Add user entity

---
+++ src/main/java/ryabchuk/sportshop/entity/User.java
@@ -0,0 +1,42 @@
+package ryabchuk.sportshop.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String password;
+
+    private String provider;
+
+    @Column(name = "provider_id")
+    private String providerId;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
+
+    @Column(name = "telegram_chat_id")
+    private String telegramChatId;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public enum Role {
+        USER, MODERATOR, ADMIN
+    }
+
+}
