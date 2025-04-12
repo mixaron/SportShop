3a3e65ce2a99db631674e187b98a8ec33177ae9d
Anton Ryabchuk <tosha.ryabchuk@gmail.com>
2025-04-12 19:15:16 +0400

    delete trash and fix

--- src/main/java/ryabchuk/sportshop/config/SecurityConfig.java
+++ src/main/java/ryabchuk/sportshop/config/SecurityConfig.java
@@ -2,8 +2,6 @@ package ryabchuk.sportshop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
