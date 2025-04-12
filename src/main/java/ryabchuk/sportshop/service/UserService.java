9d4ba634231b6f6ba7cfc40d7ddbfd5e27651891
Anton Ryabchuk <tosha.ryabchuk@gmail.com>
2025-04-12 18:08:58 +0400

    add method findByResetToken and add mail methods to UserService

--- src/main/java/ryabchuk/sportshop/service/UserService.java
+++ src/main/java/ryabchuk/sportshop/service/UserService.java
@@ -1,6 +1,9 @@
 package ryabchuk.sportshop.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ryabchuk.sportshop.dto.UserDto;
@@ -8,12 +11,16 @@ import ryabchuk.sportshop.mapper.UserMapper;
 import ryabchuk.sportshop.model.User;
 import ryabchuk.sportshop.repository.UserRepository;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Service
 @AllArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
+    private final JavaMailSender mailSender;
 
     public void register(UserDto userDto) {
         if (userRepository.existsByEmail(userDto.getEmail())) {
@@ -24,5 +31,41 @@ public class UserService {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         userRepository.save(user);
+    }
+
+    public void sendPasswordResetLink(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        String token = UUID.randomUUID().toString();
+        user.setResetToken(token);
+        user.setResetTokenExpiry(LocalDateTime.now().plusHours(1));
+
+        userRepository.save(user);
+
+        String resetLink = "http://localhost:8989/auth/reset-password?token=" + token;
+        sendEmail(user.getEmail(), "Password Reset", "Click to reset: " + resetLink);
+    }
+
+    public void resetPassword(String token, String newPassword) {
+        User user = userRepository.findByResetToken(token)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid token"));
+
+        if (user.getResetTokenExpiry().isBefore(LocalDateTime.now())) {
+            throw new IllegalArgumentException("Token expired");
+        }
+        user.setPassword(passwordEncoder.encode(newPassword));
+        user.setResetToken(null);
+        user.setResetTokenExpiry(null);
+
+        userRepository.save(user);
+    }
+
+    private void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
     }
 }
