f278ba7e666570063f696ea57ed35c2f2c314681
Anton Ryabchuk <tosha.ryabchuk@gmail.com>
2025-04-14 00:30:02 +0400

    Change CustomAuthenticationSuccessHandler because successfulurl didnt work

--- src/main/java/ryabchuk/sportshop/handler/CustomAuthenticationSuccessHandler.java
+++ src/main/java/ryabchuk/sportshop/handler/CustomAuthenticationSuccessHandler.java
@@ -1,26 +1,28 @@
 package ryabchuk.sportshop.handler;
 
-import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 import ryabchuk.sportshop.config.LoginAttemptService;
 
 import java.io.IOException;
+import java.util.Collection;
 
 @Component
 public class CustomAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
     private final LoginAttemptService loginAttemptService;
 
+    @Autowired
     public CustomAuthenticationSuccessHandler(LoginAttemptService loginAttemptService) {
         this.loginAttemptService = loginAttemptService;
+        setDefaultTargetUrl("/products");
+        setAlwaysUseDefaultTargetUrl(true);
     }
 
     @Override
@@ -30,6 +32,16 @@ public class CustomAuthenticationSuccessHandler extends SavedRequestAwareAuthent
             throws ServletException, IOException {
         String username = authentication.getName();
         loginAttemptService.loginSucceeded(username);
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        boolean isAdmin = authorities.stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+        boolean isModerator = authorities.stream().anyMatch(a -> a.getAuthority().equals("ROLE_MODERATOR"));
+
+        if (isAdmin || isModerator) {
+            setDefaultTargetUrl("/admin");
+        } else {
+            setDefaultTargetUrl("/products");
+        }
 
         super.onAuthenticationSuccess(request, response, authentication);
     }
