690be5aabd72f40a49a926b7b7c0d2a2789fd6d5
Anton Ryabchuk <tosha.ryabchuk@gmail.com>
2025-04-15 01:00:35 +0400

    Create userController

---
+++ src/main/java/ryabchuk/sportshop/controller/user/UserController.java
@@ -0,0 +1,38 @@
+package ryabchuk.sportshop.controller.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ryabchuk.sportshop.config.user.CustomUserDetails;
+import ryabchuk.sportshop.dto.UserDto;
+import ryabchuk.sportshop.service.UserService;
+
+@Controller
+@RequestMapping("/profile")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public String viewProfile(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
+        model.addAttribute("user", userService.getUserById(userDetails.getId()));
+        return "profile/view";
+    }
+
+    @GetMapping("/edit")
+    public String editUser(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
+        model.addAttribute("user", userService.getUserById(userDetails.getId()));
+        return "profile/edit";
+    }
+
+    @PostMapping("/edit")
+    public String updateUser(@AuthenticationPrincipal CustomUserDetails userDetails, @ModelAttribute UserDto userDto) {
+        userService.updateUser(userDetails.getId(), userDto);
+        return "redirect:/profile";
+    }
+}
