833bd95f9bb8aefdd17be886b186d7e61d30440a
Anton Ryabchuk <tosha.ryabchuk@gmail.com>
2025-04-15 01:03:09 +0400

    add repo annotation

--- src/main/java/ryabchuk/sportshop/repository/CategoryRepository.java
+++ src/main/java/ryabchuk/sportshop/repository/CategoryRepository.java
@@ -1,10 +1,12 @@
 package ryabchuk.sportshop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ryabchuk.sportshop.model.Category;
 
 import java.util.Optional;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByName(String name);
 }
