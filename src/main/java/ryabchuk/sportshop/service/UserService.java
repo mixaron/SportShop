1d4183d7ae70e0580e777f2e52d32ce77fc4928b
Anton Ryabchuk <tosha.ryabchuk@gmail.com>
2025-04-21 00:20:31 +0400

    add method saveUser in UserService

--- src/main/java/ryabchuk/sportshop/service/UserService.java
+++ src/main/java/ryabchuk/sportshop/service/UserService.java
@@ -119,4 +119,8 @@ public class UserService {
         user.setFirstName(userDto.getFirstName());
         user.setLastName(userDto.getLastName());
     }
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
 }
