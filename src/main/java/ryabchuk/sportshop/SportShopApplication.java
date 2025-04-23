7e4cc92ef6e1ce01efaf68a7d7f1d1f981058c4b
Anton Ryabchuk <tosha.ryabchuk@gmail.com>
2025-04-23 19:27:48 +0400

    Add patch put delete methods

--- src/main/java/ryabchuk/sportshop/SportShopApplication.java
+++ src/main/java/ryabchuk/sportshop/SportShopApplication.java
@@ -2,6 +2,11 @@ package ryabchuk.sportshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SportShopApplication {
@@ -9,5 +14,10 @@ public class SportShopApplication {
     public static void main(String[] args) {
         SpringApplication.run(SportShopApplication.class, args);
     }
-
+    @Bean
+    public FilterRegistrationBean hiddenHttpMethodFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new HiddenHttpMethodFilter());
+        filterRegistrationBean.setUrlPatterns(Arrays.asList("/*"));
+        return filterRegistrationBean;
+    }
 }
